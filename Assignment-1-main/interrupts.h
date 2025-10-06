789644a16ab77880e8e28eddef41e3ea4d38bf10
npono21 <nickpon123@gmail.com>
2025-10-05 20:36:55 -0400

    final changes

--- Assignment-1-main/interrupts.h
+++ Assignment-1-main/interrupts.h
@@ -11,28 +11,23 @@
 #define TIME_TO_FIND_VECTOR 1
 #define TIME_TO_LOAD_ADDRESS 1
 
-// Defining our VectorTable struct which will contain all Vector Table addresses we need
+// define VectorTable struct containing vectors and their addresses
 typedef struct
 {
     int address;
     int vector;
 } VectorTable;
 
-// Reads our defined Vector Table address and write them into the VectorTable struct
 void createVectorTable(VectorTable vectorTable[], FILE *vectorTableFile, int lineCount);
 
-// This function will take the operation from trace.txt, ex. 'CPU,', and remove the commma for better processing later on
 void removeCommas(const char *input, char *output);
 
 char *getExecutionTime(char *input);
 
-// Function to handle CPU operations
 void handleCPU(FILE *file, int *currentTime, int executionTime);
 
-// Function to handle SYSCALL operations
 void handleSysCall(FILE *file, int *currentTime, int executionTime, char *filteredOperation, VectorTable vectorTable[]);
 
-// Function to handle END_IO operations
 void handleEndIO(FILE *file, int *currentTime, int executionTime, char *filteredOperation, VectorTable vectorTable[]);
 
 #endif
